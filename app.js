8c959cbb4001428bd652f572973d2688fd3a266c
Lasha <babaxatsulukidze@gmail.com>
2024-02-19 16:03:02 +0400

    feat: added login and message send logics

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-
-import pg from "pg";
 import "dotenv/config";
+import pg from "pg";
+
 const { Pool } = pg;
 
 const app = express();
@@ -12,32 +12,83 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const pool = new Pool({
-  host: 'localhost',
+  host: "localhost",
   port: 5432,
-  database: 'ChatApp',
-  user: 'postgres',
-  password: 'lasha',
+  database: "ChatApp",
+  user: "postgres",
+  password: "lasha",
 });
 
-app.get('/', async (req,res,next)=>{
-    const client = await pool.connect();
-    const result = await client.query('select (userid, username, email, password_hash, created_at) from users');
-    console.log(result.rows)
-    res.json(result.rows);
-})
 
-
-//message input
-app.post('/', async (req, res , next)=>{
+//login
+app.post("/main/login", async (req, res, next) => {
   const data = req.body;
   const client = await pool.connect();
-  const result = await client.query(
-    "INSERT INTO Messages (senderid, receiverid, content) VALUES ($1,$2,$3)",
-    [data.senderid, data.reciverid, data.content]
-  );
-  res.status(200).send("message sent")
-})
+  try{
+    const result = await client.query(
+      "SELECT username FROM Users WHERE username=$1 AND password_hash=$2",
+      [data.username, data.password_hash]
+    );
+    res.status(200).json(result.rows);
+  }catch(error){
+    res.status(500).json({error})
+  }finally{
+    client.release()
+  }
+});
 
+//get all users
+app.post("/main/chat", async (req, res, next) => {
+  const data = req.body;
+  const client = await pool.connect();
+  try{
+    const result = await client.query(
+      "SELECT username, usersurname, profileimage FROM Users WHERE username != $1",
+      [data.username]
+    );
+    res.status(200).json(result.rows);
+  }catch(error){
+    res.status(500).json({error})
+  }finally{
+    client.release()
+  }
+});
 
+//message input
+app.post("/main/chatroom/send", async (req, res, next) => {
+  const data = req.body;
+  const client = await pool.connect();
+  try{
+    const result = await client.query(
+      "INSERT INTO Messages (senderid, receiverid, content) VALUES ($1,$2,$3)",
+      [data.senderid, data.receiverid, data.content]
+    );
+    res.status(200).json("message sent");
+  }catch(error){
+    console.log(error)
+    res.status(500).json(error)
+  }finally{
+    client.release();
+  }
+});
 
-app.listen(4000,()=>{console.log(`app is listening on port 4000`)})
+//get all messages
+app.post("/main/chatroom", async (req, res, next) => {
+  const data = req.body;
+  const client = await pool.connect();
+  try{
+    const result = await client.query(
+      "SELECT * FROM Messages WHERE (senderid = $1 AND receiverid = $2) OR (senderid = $2 AND receiverid = $1) ORDER BY timestamp",
+      [data.user1, data.user2]
+    );
+    res.status(200).json(result.rows);
+  }catch(eror){
+    res.status(500).json({eror})
+  }finally{
+    client.release();
+  }
+});
+
+app.listen(4000, () => {
+  console.log(`app is listening on port 4000`);
+});
